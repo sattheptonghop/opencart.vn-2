cd8f22556d66fb45c5823054454c8ef7a91eb534
shanksimi <shank.0.hair@gmail.com>
2016-09-06 09:33:37 +0700

    Re-initial Commit

--- catalog/language/english/news/news.php
+++
@@ -1,4 +0,0 @@
-<?php
-$_['text_published_at']    = 'Published at';
-$_['text_on']    = 'on';
-$_['text_views']    = 'Views';
--- catalog/language/vietnamese/news/news.php
+++
@@ -1,4 +0,0 @@
-<?php
-$_['text_published_at']    = 'Đăng lúc';
-$_['text_on']    = 'ngày';
-$_['text_views']    = 'Lượt xem';
